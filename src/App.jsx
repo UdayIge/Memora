6cf81e56e4ea9a1524c0e3e3f691792d2fe45e62
Uday Ige <udayige1696@gmail.com>
2025-08-05 22:26:39 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-3xl text-blue-700 flex justify-center font-bold'>Memora</div>
+    </>
+  )
+}
+
+export default App
